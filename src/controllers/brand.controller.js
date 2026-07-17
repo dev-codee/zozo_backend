6a707e1c802da572856fc0e0881eafa2058ed43d
Faizan Ahmad <faizanahd289@gmail.com>
2026-07-18 02:03:14 +0500

    implement getBrands controller to fix empty frontend brand filters

--- src/controllers/brand.controller.js
+++ src/controllers/brand.controller.js
@@ -4,13 +4,16 @@ import * as brandService from '../services/brand.service.js';
 import * as phoneService from '../services/phone.service.js';
 
 export const getBrands = asyncHandler(async (req, res) => {
-    // Calls brandService.getAllBrands
-    res.status(200).json(new ApiResponse(200, [], "Brands fetched successfully"));
+    const brands = await brandService.getAllBrands();
+    res.status(200).json(new ApiResponse(200, brands, "Brands fetched successfully"));
 });
 
 export const getBrandBySlug = asyncHandler(async (req, res) => {
-    // Calls brandService.getBrandBySlug
-    res.status(200).json(new ApiResponse(200, {}, "Brand fetched successfully"));
+    const brand = await brandService.getBrandBySlug(req.params.slug);
+    if (!brand) {
+        return res.status(404).json(new ApiResponse(404, null, "Brand not found"));
+    }
+    res.status(200).json(new ApiResponse(200, brand, "Brand fetched successfully"));
 });
 
 export const getBrandPhones = asyncHandler(async (req, res) => {
