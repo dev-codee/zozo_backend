35694a60ba0fc807546af4a24ccefbe91933f76f
Faizan Ahmad <faizanahd289@gmail.com>
2026-07-18 20:57:49 +0500

    admin dashboard added8

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'zozosecretkey123';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const protectAdmin = (req, res, next) => {
   let token;
