ebcc27198dfe8a0a3660c5b74013e62516384c00
Faizan Ahmad <faizanahd289@gmail.com>
2026-07-29 01:52:38 +0500

    review fix

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
